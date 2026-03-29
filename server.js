58fb1119381715c53c23b5d0ef7379b6e0d0b257
Hussain Abbas Ahmed Alhakeem <halhakeem2@gmail.com>
2026-03-29 11:00:00 +0300

    install everthing and put in the server

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 require("dotenv").config({ quiet: true })
 const express = require("express")
 const morgan = require("morgan")
+const methodOverride = require("method-override")
+
+const session = require("express-session")
+
+const { MongoStore } = require("connect-mongo")
+
+const path = require("path")
 
 const dns = require("dns")
 dns.setServers(["8.8.8.8", "1.1.1.1"])
 
 const db = require("./db")
+
 const PORT = 3000
 
 const app = express()
-app.use(express.json())
-app.use(morgan("dev"))
 
-app.use("/", (req, res) => {
-  res.send("hello world")
-})
+const middleware = require("./middleware")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(morgan("dev"))
+app.use(methodOverride("_method"))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+)
+app.use(middleware.passUserToView)
 
 app.listen(PORT, () => {
-  console.log("💻 server is cooking . . . .")
+  console.log(`💻 server is cooking ${PORT} ....  `)
 })
